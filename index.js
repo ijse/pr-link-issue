d6b8356d2e0dbeef6fbe5db11108433e429dae1e
ijse <i@ijser.cn>
2017-10-11 13:07:46 +0800

    update keywords

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function detectIssueId (ref) {
-    const reg = /^(feature|fix|improve|solve|update)-(\d+)/i
+    const reg = /^(feature|fix|hotfix|improve|solve|update)-(\d+)/i
     const matches = reg.exec(ref) || []
     return matches[2]
 }
